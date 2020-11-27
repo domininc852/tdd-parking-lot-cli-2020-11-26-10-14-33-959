8945aaf9ad0f285a9aa0f0da214a226cd99af3d9
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-11-27 23:08:52 +0800

    [ParkingBoy-Story5][Dominic Leung] test: implement test for park to parking lot according to available position rate when park multiple cars given all parking slots have different capacity and different available slots

--- src/test/java/com/oocl/cultivation/SuperSuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSuperSmartParkingBoyTest.java
@@ -30,4 +30,33 @@ public class SuperSuperSmartParkingBoyTest {
         assertEquals(3, parkingLot1.getAvailableSpace());
         assertEquals(1, parkingLot2.getAvailableSpace());
     }
+    @Test
+    public void should_park_to_parking_lot_according_to_available_position_rate_when_park_multiple_cars_given_parking_lots_with_different_capacity_and_available_slot() throws NotEnoughPositionException {
+        //given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        Car car4 = new Car();
+        Car car5 = new Car();
+        Car car6 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(6);
+        ParkingLot parkingLot2 = new ParkingLot(8);
+        ParkingLot parkingLot3 = new ParkingLot(10);
+        parkingLot3.park(car1);
+        parkingLot3.park(car2);
+        parkingLot1.park(car3);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        parkingLots.add(parkingLot3);
+        SuperSmartParkingBoy parkingBoy = new SuperSmartParkingBoy(parkingLots);
+        //when
+        parkingBoy.parkCar(car4);
+        parkingBoy.parkCar(car5);
+        parkingBoy.parkCar(car6);
+        //then
+        assertEquals(4, parkingLot1.getAvailableSpace());
+        assertEquals(6, parkingLot2.getAvailableSpace());
+        assertEquals(8, parkingLot3.getAvailableSpace());
+    }
 }
