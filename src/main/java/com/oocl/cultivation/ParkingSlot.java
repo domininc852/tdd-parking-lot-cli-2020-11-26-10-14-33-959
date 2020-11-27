60f8a070514a877234729e22b4feead38ddbd922
NSC/Network Service Center <NSC@oocl.com>
2020-11-27 15:19:57 +0800

    [ParkingBoy][Dominic Leung] test: implement test for return null when park car given car and parking slot with no available slot

--- src/main/java/com/oocl/cultivation/ParkingSlot.java
+++ src/main/java/com/oocl/cultivation/ParkingSlot.java
@@ -1,7 +1,17 @@
 package com.oocl.cultivation;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingSlot {
+    private final int capacity;
+    private List<Car> cars;
+    public ParkingSlot(int capacity) {
+        this.capacity = capacity;
+        cars=new ArrayList<>();
+    }
+
     public Ticket park(Car car) {
         return new Ticket();
     }
--- src/test/java/com/oocl/cultivation/ParkingSlotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingSlotTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParkingSlotTest {
     //given car and parking lot has space
@@ -13,10 +14,24 @@ public class ParkingSlotTest {
     public void should_return_ticket_when_park_car_given_car_parking_slot_with_available_slot() {
         //given
         Car car = new Car();
-        ParkingSlot parkingSlot = new ParkingSlot();
+        ParkingSlot parkingSlot = new ParkingSlot(1);
         //when
         Ticket ticket = parkingSlot.park(car);
         //then
         assertNotNull(ticket);
+    }
+    //given car and parking lot does not have space
+    //when parking car
+    //then
+    //return null
+    @Test
+    public void should_return_null_when_park_car_given_car_parking_slot_with_no_available_slot() {
+        //given
+        Car car = new Car();
+        ParkingSlot parkingSlot = new ParkingSlot(0);
+        //when
+        Ticket ticket = parkingSlot.park(car);
+        //then
+        assertNull(ticket);
     }
 }
