d2f3c667217af5a73737d6d3ed1e92d8ebaef7b3
NSC/Network Service Center <NSC@oocl.com>
2020-11-27 15:02:15 +0800

    [ParkingBoy][Dominic Leung] test: implement test for parked once given parking boy, car and parking slot with available slot

---
+++ src/main/java/com/oocl/cultivation/ParkingSlot.java
@@ -0,0 +1,6 @@
+package com.oocl.cultivation;
+
+public class ParkingSlot {
+    public void park() {
+    }
+}
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,10 +1,21 @@
 package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.times;
 
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_be_parked_once_when_park_a_car_given_car_parking_boy_parking_slot_with_available_slot() {
+        //given
+        Car car = new Car();
+        ParkingSlot parkingSlot = new ParkingSlot();
+        ParkingBoy parkingBoy = new ParkingBoy();
+        //when
+        parkingBoy.park();
+        //then
+        Mockito.verify(parkingSlot,times(1)).park();
 
     }
 }
